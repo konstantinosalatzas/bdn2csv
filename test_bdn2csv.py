1e09b6df092ae7dad6eca4b6ac5b5e1488667b7f
Konstantinos Alatzas <kpalatzas@windowslive.com>
2024-12-06 18:23:20 +0000

    Format

--- test_bdn2csv.py
+++ test_bdn2csv.py
@@ -6,10 +6,13 @@ class Test(unittest.TestCase):
     def test_convert(self):
         xml_path = "/workspaces/bdn2csv/data/Export.xml"
         csv_path = "/workspaces/bdn2csv/data/Test.csv"
+
         df = bdn2csv.convert(xml_path, csv_path)
+
         out = pd.read_csv(csv_path) # Output DataFrame
         ans = pd.read_csv("/workspaces/bdn2csv/data/Import.csv") # Expected dataframe
         cmp = out.compare(ans)
+
         self.assertEqual(len(cmp.index), 0)
 
 if __name__ == "__main__":
