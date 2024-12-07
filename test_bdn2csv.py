c70865636f3b75143fbafae92f574328f7e508bf
Konstantinos Alatzas <kpalatzas@windowslive.com>
2024-12-07 09:00:26 +0000

    Implement TestParseTypes

--- test_bdn2csv.py
+++ test_bdn2csv.py
@@ -12,10 +12,19 @@ class TestConvert(unittest.TestCase):
         cmp = out.compare(ans)
         self.assertEqual(len(cmp.index), 0)
 
-class TestParse(unittest.TestCase):
+class TestParseTypes(unittest.TestCase):
     def test_parse_types(self):
-        pass
+        xml_path = "/workspaces/bdn2csv/data/Export.xml"
+        bdn = bdn2csv.BDN(xml_path)
+        bdn.parse_types()
+        list_out = bdn.std_attrs + bdn.non_std_attrs # Output list
+        list_out = [attr.replace("Type_", "Type") for attr in list_out] # the case when "Type" is in standard AND non-standard attributes
+        df = pd.read_csv("/workspaces/bdn2csv/data/Import.csv")
+        list_ans = df.columns.tolist() # Expected list
+        list_ans = [attr.replace("Type.1", "Type") for attr in list_ans] # the case when "Type" in in standard AND non-standard attributes
+        self.assertListEqual(list_out, list_ans)
 
+class TestParseValues(unittest.TestCase):
     def test_parse_values(self):
         pass
 
