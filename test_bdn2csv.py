147db4a01faaf559b73428987a66684c6858ce02
Konstantinos Alatzas <kpalatzas@windowslive.com>
2024-12-07 07:48:33 +0000

    Initialize TestParse

--- test_bdn2csv.py
+++ test_bdn2csv.py
@@ -2,18 +2,22 @@ import unittest
 import bdn2csv
 import pandas as pd
 
-class Test(unittest.TestCase):
+class TestConvert(unittest.TestCase):
     def test_convert(self):
         xml_path = "/workspaces/bdn2csv/data/Export.xml"
         csv_path = "/workspaces/bdn2csv/data/Test.csv"
-
         df = bdn2csv.convert(xml_path, csv_path)
-
         out = pd.read_csv(csv_path) # Output DataFrame
-        ans = pd.read_csv("/workspaces/bdn2csv/data/Import.csv") # Expected dataframe
+        ans = pd.read_csv("/workspaces/bdn2csv/data/Import.csv") # Expected DataFrame
         cmp = out.compare(ans)
-
         self.assertEqual(len(cmp.index), 0)
+
+class TestParse(unittest.TestCase):
+    def test_parse_types(self):
+        pass
+
+    def test_parse_values(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
