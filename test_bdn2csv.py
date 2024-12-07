17c72d4e4b2db5ab081a29961f103d54821952c2
Konstantinos Alatzas <kpalatzas@windowslive.com>
2024-12-07 13:53:34 +0000

    Implement test_parse_values()

--- test_bdn2csv.py
+++ test_bdn2csv.py
@@ -18,14 +18,25 @@ class TestParseTypes(unittest.TestCase):
         bdn = bdn2csv.BDN(xml_path)
         bdn.parse_types()
         list_out = bdn.std_attrs + bdn.non_std_attrs # Output list
-        list_out = [attr.replace("Type_", "Type") for attr in list_out] # the case when "Type" is in standard AND non-standard attributes
+        list_out = [attr.replace("Type_", "Type") for attr in list_out] # when "Type" is in standard AND non-standard attributes
         df = pd.read_csv("/workspaces/bdn2csv/data/Import.csv")
         list_ans = df.columns.tolist() # Expected list
-        list_ans = [attr.replace("Type.1", "Type") for attr in list_ans] # the case when "Type" in in standard AND non-standard attributes
+        list_ans = [attr.replace("Type.1", "Type") for attr in list_ans] # when "Type" in in standard AND non-standard attributes
         self.assertListEqual(list_out, list_ans)
 
 class TestParseValues(unittest.TestCase):
-    pass
+    def test_parse_values(self):
+        xml_path = "/workspaces/bdn2csv/data/Export.xml"
+        bdn = bdn2csv.BDN(xml_path)
+        bdn.parse_types()
+        bdn.parse_values()
+        df_out = bdn.df # Output DataFrame
+        df_out = df_out.reset_index(drop=True).rename(columns={"Type_": "Type"}) # when "Type" is in standard AND non-standard attributes
+        if "Description" in df_out.columns.tolist():
+            df_out['Description'] = df_out['Description'].apply(lambda x: str(x).strip('"'))
+        df_ans = pd.read_csv("/workspaces/bdn2csv/data/Import.csv", dtype=object, keep_default_na=False) # Expected DataFrame
+        df_cmp = df_out.compare(df_ans)
+        self.assertEqual(len(df_cmp.index), 0)
 
 if __name__ == "__main__":
     unittest.main()
