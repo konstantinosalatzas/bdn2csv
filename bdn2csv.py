06c98bd980933c9691a7335bb9c5924612af2288
Konstantinos Alatzas <kpalatzas@windowslive.com>
2024-12-10 20:23:42 +0000

    Parse arguments

--- bdn2csv.py
+++ bdn2csv.py
@@ -3,7 +3,14 @@ import bdn2csv
 import pandas as pd
 
 parser = argparse.ArgumentParser()
-parser.parse_args()
+parser.add_argument("xml_path", type=str,
+                    help="XML Export file path")
+parser.add_argument("csv_path", type=str,
+                    help="CSV Import file path")
+args = parser.parse_args()
+xml_path = args.xml_path
+csv_path = args.csv_path
+print(xml_path, csv_path)
 
 '''
 xml_path = "/workspaces/bdn2csv/data/Export.xml"
