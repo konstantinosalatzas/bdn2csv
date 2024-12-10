7d7ffc6858aee2d7d91f5560d0fdb8852d85c004
Konstantinos Alatzas <kpalatzas@windowslive.com>
2024-12-10 20:44:39 +0000

    Wrap CLI with shell script

--- bdn2csv.py
+++ bdn2csv.py
@@ -2,7 +2,8 @@ import argparse
 import bdn2csv
 import pandas as pd
 
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(prog="bdn2csv",
+                                 description="Convert SAS BDN XML Export file to CSV Import file")
 parser.add_argument("xml_path", type=str,
                     help="XML Export file path")
 parser.add_argument("csv_path", type=str,
@@ -12,12 +13,7 @@ xml_path = args.xml_path
 csv_path = args.csv_path
 print(xml_path, csv_path)
 
-'''
-xml_path = "/workspaces/bdn2csv/data/Export.xml"
-csv_path = "/workspaces/bdn2csv/data/Import.csv"
-
 bdn2csv.convert(xml_path, csv_path)
 
 df = pd.read_csv(csv_path)
 print(df.head())
-'''
---
+++ run.sh
@@ -0,0 +1,1 @@
+ /home/codespace/.python/current/bin/python /workspaces/bdn2csv/bdn2csv.py "/workspaces/bdn2csv/data/Export.xml" "/workspaces/bdn2csv/data/Import.csv"
