c317a5284cb5d92a2ae5c2ec05f553caa49029fe
Konstantinos Alatzas <kpalatzas@windowslive.com>
2024-12-11 16:36:38 +0000

    Add verbosity argument

--- bdn2csv.py
+++ bdn2csv.py
@@ -8,11 +8,15 @@ parser.add_argument("xml_path", type=str,
                     help="XML Export file path")
 parser.add_argument("csv_path", type=str,
                     help="CSV Import file path")
+parser.add_argument("-v", "--verbose", action="store_true",
+                    help="Output verbosity")
 args = parser.parse_args()
+
 xml_path = args.xml_path
 csv_path = args.csv_path
 
 bdn2csv.convert(xml_path, csv_path)
 
-df = pd.read_csv(csv_path)
-print(df.head())
+if args.verbose:
+    df = pd.read_csv(csv_path)
+    print(df.head())
