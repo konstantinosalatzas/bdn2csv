e70d05d544de2c9cf0bc49159b6e03225b6dafee
Konstantinos Alatzas <kpalatzas@windowslive.com>
2024-12-18 10:21:58 +0000

    Edit setup() description

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="bdn2csv",
     version="0.1.0",
-    description="SAS BDN XML to CSV converter",
+    description="SAS BDN XML Export file to CSV Import file converter",
     author="Konstantinos Alatzas",
     url="https://github.com/konstantinosalatzas/bdn2csv",
     packages=[
