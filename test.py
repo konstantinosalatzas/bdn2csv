f1c8b52983813fa087afbdf9ae9410cfe8e6b272
Konstantinos Alatzas <kpalatzas@windowslive.com>
2024-12-30 15:08:38 +0000

    Format code

--- test.py
+++ test.py
@@ -54,14 +54,16 @@ class TestConvert(unittest.TestCase):
         bdn2csv.convert(xml_string, csv_path)
         df_out = pd.read_csv(csv_path, dtype=object, keep_default_na=False) # Output DataFrame
         # Expected DataFrame
-        df_ans = pd.DataFrame({"Name": ["Warehouse", "Loading Dock", "Section"],
-                               "Path": ["Warehouse", "Warehouse\Loading Dock", "Warehouse\Section"],
-                               "Description": ["Storage facility for goods and raw materials", "Facility for incoming and outgoing goods", "Section of the warehouse designated for a specific product or type of product"],
-                               "Requirements": ["Must meet size and security standards", "", "Must be secure and accessible"],
-                               "Status": ["Not Specified", "Not Specified", "Not Specified"],
-                               "Importance": ["Medium", "Medium", "Medium"],
-                               "Tags": ["Logistics", "", "Logistics"],
-                               "Related Terms": ["", "", "Picking"]})
+        df_ans = pd.DataFrame(
+            {"Name": ["Warehouse", "Loading Dock", "Section"],
+             "Path": ["Warehouse", "Warehouse\Loading Dock", "Warehouse\Section"],
+             "Description": ["Storage facility for goods and raw materials", "Facility for incoming and outgoing goods", "Section of the warehouse designated for a specific product or type of product"],
+             "Requirements": ["Must meet size and security standards", "", "Must be secure and accessible"],
+             "Status": ["Not Specified", "Not Specified", "Not Specified"],
+             "Importance": ["Medium", "Medium", "Medium"],
+             "Tags": ["Logistics", "", "Logistics"],
+             "Related Terms": ["", "", "Picking"]}
+        )
         df_cmp = df_out.compare(df_ans)
         self.assertEqual(len(df_cmp.index), 0)
 
