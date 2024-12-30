08347c3770c1ba292d46a85326295ebdd2d5296b
Konstantinos Alatzas <kpalatzas@windowslive.com>
2024-12-30 15:22:51 +0000

    Cleanup paths

--- bdn2csv.sh
+++ bdn2csv.sh
@@ -1,2 +1,2 @@
- /home/codespace/.python/current/bin/python /workspaces/bdn2csv/bdn2csv.py "/workspaces/bdn2csv/data/Export.xml" "/workspaces/bdn2csv/data/Import.csv"
- /home/codespace/.python/current/bin/python /workspaces/bdn2csv/bdn2csv.py -v "/workspaces/bdn2csv/data/Export.xml" "/workspaces/bdn2csv/data/Import.csv"
+python bdn2csv.py "./data/Export.xml" "./data/Import.csv"
+python bdn2csv.py -v "./data/Export.xml" "./data/Import.csv"
