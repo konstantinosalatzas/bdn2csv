7300454b4a5498a76dd3de0165f06fd05037c902
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-01-01 11:05:03 +0000

    Structure tests

--- test.py
+++ test.py
@@ -97,9 +97,11 @@ class TestConvert(unittest.TestCase):
              "Tags": ["Logistics", "", "Logistics"],
              "Related Terms": ["", "", "Picking,Warehouse\Loading Dock"]}
         )
+
         csv_path = "./data/Test.csv"
         bdn2csv.convert(xml_string, csv_path)
         df_out = pd.read_csv(csv_path, dtype=object, keep_default_na=False) # Output DataFrame
+
         df_cmp = df_out.compare(df_ans)
         self.assertEqual(len(df_cmp.index), 0)
 
@@ -203,18 +205,22 @@ class TestParse(unittest.TestCase):
     def test_parse_types(self):
         list_ans = self.df_ans.columns.tolist() # Expected list
         list_ans = [attr.replace("Type.1", "Type") for attr in list_ans] # when "Type" in in standard AND non-standard attributes
+
         self.bdn.parse_types()
         list_out = self.bdn.std_attrs + self.bdn.non_std_attrs # Output list
         list_out = [attr.replace("Type_", "Type") for attr in list_out] # when "Type" is in standard AND non-standard attributes
+        
         self.assertListEqual(list_out, list_ans)
 
     def test_parse_values(self):
         self.df_ans = self.df_ans.rename(columns={"Type": "Type_", "Type.1": "Type"}) # when "Type" is in standard AND non-standard attributes
+
         self.bdn.parse_types()
         self.bdn.parse_values()
         df_out = self.bdn.df # Output DataFrame
         if "Description" in df_out.columns.tolist():
             df_out['Description'] = df_out['Description'].apply(lambda x: str(x).strip('"'))
+        
         df_cmp = df_out.compare(self.df_ans)
         self.assertEqual(len(df_cmp.index), 0)
 
@@ -223,7 +229,9 @@ class TestAddMultipleValue(unittest.TestCase):
         values = "Picking"
         value = "Warehouse\Loading Dock"
         values_ans = "Picking,Warehouse\Loading Dock"
+        
         values_out = bdn2csv.add_multiple_value(values, value)
+        
         self.assertEqual(values_out, values_ans)
 
 if __name__ == "__main__":
