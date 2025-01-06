91f6074861f313e2b99660ca7109ef4c302b8b7a
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-01-06 15:56:53 +0000

    Add comments

--- test.py
+++ test.py
@@ -224,8 +224,8 @@ class TestParse(unittest.TestCase):
 
 class TestAddMultipleValue(unittest.TestCase):
     def test_add_multiple_value_to_empty(self):
-        values = ""
-        value = "Warehouse\Loading Dock"
+        values = "" # input values string
+        value = "Warehouse\Loading Dock" # input value string
         values_ans = "Warehouse\Loading Dock" # expected string
         
         values_out = bdn2csv.add_multiple_value(values, value) # output string
