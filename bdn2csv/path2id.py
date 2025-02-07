5d431a01c24fb55b6fb56c6302d8504cf392964e
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-02-07 06:37:00 +0000

    Add test print and call

--- bdn2csv/path2id.py
+++ bdn2csv/path2id.py
@@ -4,4 +4,7 @@ import json
 def path2id(json_path: str):
     with open(json_path, 'r') as f:
         response = json.load(f)
+    print(response)
     return
+
+path2id("/workspaces/bdn2csv/data/Response.json")
