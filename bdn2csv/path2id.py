8fcfa1c056951f77ead70aea6506ccbe4cdbe7c5
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-02-07 06:37:50 +0000

    Remove return

--- bdn2csv/path2id.py
+++ bdn2csv/path2id.py
@@ -5,6 +5,5 @@ def path2id(json_path: str):
     with open(json_path, 'r') as f:
         response = json.load(f)
     print(response)
-    return
 
 path2id("/workspaces/bdn2csv/data/Response.json")
