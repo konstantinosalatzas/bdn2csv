b7cc66a34b6c9ff2d95b75de304d8ec402a5f5ac
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-02-07 07:03:14 +0000

    Move code

--- bdn2csv/path2id.py
+++ bdn2csv/path2id.py
@@ -4,27 +4,21 @@ import json
 def path2id(json_path: str):
     with open(json_path, 'r') as f:
         response = json.load(f)
-
     names = []
     ids = []
     parentIds = []
-    parentNames = []
-
     id2name = {}
-
+    parentNames = []
     for item in response['items']:
         names.append(item['name'])
         ids.append(item['id'])
         parentIds.append(item['parentId'] if "parentId" in item else "")
         id2name[item['id']] = item['name']
-
     for item in response['items']:
         if "parentId" in item:
             parentNames.append(id2name[item['parentId']])
         else:
             parentNames.append("")
-        pass
-
     return
 
 path2id("/workspaces/bdn2csv/data/Response.json")
