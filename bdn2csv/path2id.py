169156b927dca5986f65f0b7c159291cfc94cf81
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-02-09 05:08:25 +0000

    Implement id2name()

--- bdn2csv/path2id.py
+++ bdn2csv/path2id.py
@@ -1,6 +1,28 @@
 import pandas as pd
 import json
 
+def id2name(response):
+    names = [] # term names
+    ids = [] # term IDs
+    parentIds = [] # term parent IDs
+    id2name = {} # map term IDs to names
+    for item in response['items']:
+        name = item['name']
+        id = item['id']
+        parentId = (item['parentId'] if "parentId" in item else "")
+        names.append(name)
+        ids.append(id)
+        parentIds.append(parentId)
+        id2name[id] = name
+
+    parentNames = [] # term parent names
+    for item in response['items']:
+        parentName = (id2name[item['parentId']] if "parentId" in item else "")
+        parentNames.append(parentName)
+
+    df = pd.DataFrame({"name": names, "id": ids, "parentId": parentIds, "parentName": parentNames})
+    return df
+
 def path2id(json_path: str) -> pd.DataFrame:
     with open(json_path, 'r') as f:
         response = json.load(f) # GET /terms response JSON
