870623a934c05b8921f90f9b5d75d9e4eed4251b
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-02-09 05:12:52 +0000

    Refactor with id2name()

--- bdn2csv/path2id.py
+++ bdn2csv/path2id.py
@@ -27,27 +27,10 @@ def path2id(json_path: str) -> pd.DataFrame:
     with open(json_path, 'r') as f:
         response = json.load(f) # GET /terms response JSON
 
-    names = [] # term names
-    ids = [] # term IDs
-    parentIds = [] # term parent IDs
-    id2name = {} # map term IDs to names
-    for item in response['items']:
-        name = item['name']
-        id = item['id']
-        parentId = (item['parentId'] if "parentId" in item else "")
-        names.append(name)
-        ids.append(id)
-        parentIds.append(parentId)
-        id2name[id] = name
-
-    parentNames = [] # term parent names
-    for item in response['items']:
-        parentName = (id2name[item['parentId']] if "parentId" in item else "")
-        parentNames.append(parentName)
-
-    df = pd.DataFrame({"name": names, "id": ids, "parentId": parentIds, "parentName": parentNames})
+    df = id2name(response)
     print(df.head()) #dev
 
+    ids = df['id'].values.tolist()
     paths = [] # term paths
     for _, term in df.iterrows():
         path = term['name']
