df7805cc46e48bebdc3e4a71854ee0c22251244b
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-02-12 05:11:08 +0000

    Define arguments

--- path2id.py
+++ path2id.py
@@ -1,3 +1,10 @@
 import argparse
 import bdn2csv
 import pandas as pd
+
+parser = argparse.ArgumentParser(prog="path2id")
+parser.add_argument("json_path", type=str,
+                    help="GET /terms response JSON file path")
+parser.add_argument("-v", "--verbose", action="store_true",
+                    help="activate output verbosity")
+args = parser.parse_args()
