31b8b3c91fd038a9ac7cfa6509ec2ed510dcda39
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-02-12 08:10:39 +0000

    Add comments

--- bdn2csv.py
+++ bdn2csv.py
@@ -4,22 +4,27 @@ import pandas as pd
 
 parser = argparse.ArgumentParser(prog="bdn2csv",
                                  description="Convert SAS BDN XML Export file to CSV Import file")
+
 parser.add_argument("in_path", type=str,
                     help="input file path")
 parser.add_argument("out_path", type=str,
                     help="output file path")
+
 parser.add_argument("--path2id", action="store_true")
 parser.add_argument("-v", "--verbose", action="store_true",
                     help="activate output verbosity")
+
 args = parser.parse_args()
 
 in_path = args.in_path
 out_path = args.out_path
 
+# convert
 if not args.path2id:
     bdn2csv.convert(in_path, out_path)
     df = pd.read_csv(out_path)
 
+# path2id
 if args.path2id:
     df = bdn2csv.path2id(in_path)
     df.to_csv(out_path, index=False)
