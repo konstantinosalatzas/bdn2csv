7b22625010de23453448fdafddb90769e73ea327
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-02-25 09:43:28 +0000

    Add comment

--- bdn2csv/viz.py
+++ bdn2csv/viz.py
@@ -3,7 +3,7 @@ import networkx as nx
 
 class Viz:
     def __init__(self):
-        self.G = nx.DiGraph()
+        self.G = nx.DiGraph() # BDN as a directed graph
 
     def visualize(self):
         pass
