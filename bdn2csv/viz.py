8f724cc04cb2d02cbdede0186954a096bce06db5
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-02-27 07:46:10 +0000

    Add type hint

--- bdn2csv/viz.py
+++ bdn2csv/viz.py
@@ -5,7 +5,7 @@ class BDN:
     def __init__(self):
         self.G = nx.DiGraph() # BDN as a directed graph
 
-    def construct(self, csv_path):
+    def construct(self, csv_path: str):
         G = self.G
         df = pd.read_csv(csv_path)
         for _, term in df.iterrows():
