48a73c20b41bdfdac825d2fc6ae50907088c3cf6
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-03-05 06:37:29 +0000

    Add labels to related term edges

--- bdn2csv/viz.py
+++ bdn2csv/viz.py
@@ -40,8 +40,8 @@ class BDN:
                 related_term_path = get_related_term_path(related_term)
                 related_term_label = get_related_term_label(related_term)
                 if related_term_path in G:
-                    G.add_edge(term['Path'], related_term_path)
-                    G.add_edge(related_term_path, term['Path'])
+                    G.add_edge(term['Path'], related_term_path, label=related_term_label)
+                    G.add_edge(related_term_path, term['Path'], label=related_term_label)
 
     def visualize(self):
         pass
