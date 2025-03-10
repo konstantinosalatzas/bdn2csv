fadc9f078f5d6948019bdf87c7025975298f2bdc
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-03-10 13:02:28 +0000

    Add viz command to wrapper shell script

--- bdn2csv.sh
+++ bdn2csv.sh
@@ -1,2 +1,3 @@
 python bdn2csv.py -v "./data/Export.xml" "./data/Import.csv"
 python bdn2csv.py --path2id -v "./data/Response.json" "./data/path2id.csv"
+python bdn2csv.py --viz "./data/Import.csv" "./data/viz.png"
