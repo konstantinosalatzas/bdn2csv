ebe9f6528930a7e438f9ff2ae409feaffd8cef9d
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-03-19 18:00:24 +0000

    Initialize test_construct()

--- test.py
+++ test.py
@@ -294,6 +294,9 @@ class TestPath2Id(unittest.TestCase):
         self.assertEqual(len(df_cmp.index), 0)
 
 class TestViz(unittest.TestCase):
+    def test_construct():
+        pass
+
     def test_get_parent_path_for_leaf(self):
         term_path = "Warehouse\\Section"
         parent_path_ans = "Warehouse"
