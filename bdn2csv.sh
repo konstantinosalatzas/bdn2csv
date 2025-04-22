e5d05549c47805cffb33ac5f93761ea57d5646bb
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-04-22 12:14:06 +0000

    Comment command substitution

--- bdn2csv.sh
+++ bdn2csv.sh
@@ -1,2 +1,4 @@
 python bdn2csv.py ./data/Export.xml ./data/Import.csv
 python bdn2csv.py --viz ./data/Import.csv ./data/viz.png
+
+#python bdn2csv.py --viz $(python bdn2csv.py ./data/Export.xml ./data/Import.csv) ./data/viz.png
