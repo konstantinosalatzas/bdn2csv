b0cbb38c89f5d43d08c04e39bb04f8beecaae910
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-04-23 12:58:23 +0000

    Add comments

--- bdn2csv.sh
+++ bdn2csv.sh
@@ -1,4 +1,4 @@
-python bdn2csv.py ./data/Export.xml ./data/Import.csv
-python bdn2csv.py --viz ./data/Import.csv ./data/viz.png
+python bdn2csv.py ./data/Export.xml ./data/Import.csv # convert
+python bdn2csv.py --viz ./data/Import.csv ./data/viz.png # visualize
 
 #python bdn2csv.py --viz $(python bdn2csv.py ./data/Export.xml ./data/Import.csv) ./data/viz.png
