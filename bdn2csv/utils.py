c56bfa9e66b9ddf7b7f80b088c71a09763db6500
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-06-06 05:18:54 +0000

    Remove class extensions

--- bdn2csv/utils.py
+++ bdn2csv/utils.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
-class bdn_key_to_value(dict):
+class bdn_key_to_value:
+    def __init__(self):
+        pass
+
     def lookup(self):
         pass
 
-class bdn_set(set):
+class bdn_set:
+    def __init__(self):
+        pass
+
     def isin(self):
         pass
