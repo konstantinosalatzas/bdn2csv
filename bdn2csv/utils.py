7d01688002238809573b73ced7841186407bd67b
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-06-06 11:53:19 +0000

    Add comment

--- bdn2csv/utils.py
+++ bdn2csv/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 
+# Utility class to lookup BDN DataFrame rows by key columns
 class BDN_dict:
     def __init__(self, df: pd.DataFrame, key: str) -> dict[str, pd.Series]:
         self.key_to_row = {row[key]: row for _, row in df.iterrows()} # Map keys to rows
