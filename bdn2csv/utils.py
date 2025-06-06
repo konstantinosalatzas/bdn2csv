158f17d3d38bd47d5b3263ea9c26ed14aca0f1b7
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-06-06 11:43:51 +0000

    Rename class

--- bdn2csv/utils.py
+++ bdn2csv/utils.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-class bdn_dict:
+class BDN_dict:
     def __init__(self, df: pd.DataFrame, key: str) -> dict[str, pd.Series]:
         self.key_to_row = {row[key]: row for _, row in df.iterrows()}
 
