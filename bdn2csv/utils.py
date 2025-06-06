7761b8444d06de4c89a2d25f0dfb12d9a012492d
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-06-06 11:38:44 +0000

    Add type hint

--- bdn2csv/utils.py
+++ bdn2csv/utils.py
@@ -4,7 +4,7 @@ class bdn_dict:
     def __init__(self, df: pd.DataFrame, key: str) -> dict[str, pd.Series]:
         self.key_to_row = {row[key]: row for _, row in df.iterrows()}
 
-    def look_up(self, key) -> pd.Series:
+    def look_up(self, key: str) -> pd.Series:
         key_to_row = self.key_to_row
         row = key_to_row.get(key, pd.Series(dtype='float64'))
         return row
