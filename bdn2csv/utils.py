ff8b733605685b1cdca52abb4b0a1635bea0abed
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-06-06 11:38:10 +0000

    Implement lookup

--- bdn2csv/utils.py
+++ bdn2csv/utils.py
@@ -4,8 +4,10 @@ class bdn_dict:
     def __init__(self, df: pd.DataFrame, key: str) -> dict[str, pd.Series]:
         self.key_to_row = {row[key]: row for _, row in df.iterrows()}
 
-    def look_up(self) -> pd.Series:
-        pass
+    def look_up(self, key) -> pd.Series:
+        key_to_row = self.key_to_row
+        row = key_to_row.get(key, pd.Series(dtype='float64'))
+        return row
 
 class bdn_set:
     def __init__(self, df: pd.DataFrame, key: str) -> set[str]:
