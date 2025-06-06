66a8e86537950578778ac6a92e1c8d69c7a16380
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-06-06 11:31:58 +0000

    Implement __init__

--- bdn2csv/utils.py
+++ bdn2csv/utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 class bdn_dict:
     def __init__(self, df: pd.DataFrame, key: str) -> dict[str, pd.Series]:
-        pass
+        self.key_to_row = {row[key]: row for _, row in df.iterrows()}
 
     def look_up(self):
         pass
