882e41aa8bd0b898ef3468b42e8134519cd0b6e8
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-06-06 07:51:18 +0000

    Update type hints

--- bdn2csv/utils.py
+++ bdn2csv/utils.py
@@ -1,14 +1,14 @@
 import pandas as pd
 
 class bdn_dict:
-    def __init__(self, df: pd.DataFrame, key: str) -> dict:
+    def __init__(self, df: pd.DataFrame, key: str) -> dict[str, pd.Series]:
         pass
 
     def look_up(self):
         pass
 
 class bdn_set:
-    def __init__(self, df: pd.DataFrame, key: str) -> set:
+    def __init__(self, df: pd.DataFrame, key: str) -> set[str]:
         pass
 
     def is_in(self):
