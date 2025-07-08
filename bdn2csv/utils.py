a39fd3207c027b56476fa9ed09fc4c1bd6020a65
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-07-08 11:19:25 +0000

    Move functions to utils

--- bdn2csv/utils.py
+++ bdn2csv/utils.py
@@ -1,5 +1,17 @@
 import pandas as pd
 
+def get_parent_path(term_path: str) -> str:
+    parent_path = "\\".join(term_path.split("\\")[0:-1])
+    return parent_path
+
+def get_related_term_path(related_term: str) -> str:
+    related_term_path = related_term.split("|")[0]
+    return related_term_path
+
+def get_related_term_label(related_term: str) -> str:
+    related_term_label = related_term.split("|")[1] if "|" in related_term else "" # if related term has label
+    return related_term_label
+
 # Utility class to lookup BDN DataFrame rows by key columns
 class BDN_dict:
     def __init__(self, df: pd.DataFrame, key: str) -> dict[str, pd.Series]:
