1b9d051572ee13bd454408c2d42dcdaed1bfcfb6
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-07-08 11:22:50 +0000

    Move function imports

--- bdn2csv/__init__.py
+++ bdn2csv/__init__.py
@@ -1,5 +1,5 @@
 from bdn2csv.convert import convert
 from bdn2csv.BDN import BDN, add_multiple_value
 from bdn2csv.path2id import path2id, id2name, id2path
-from bdn2csv.viz import visualize, BDNx, get_parent_path, get_related_term_path, get_related_term_label
-from bdn2csv.utils import BDN_dict
+from bdn2csv.viz import visualize, BDNx
+from bdn2csv.utils import BDN_dict, get_parent_path, get_related_term_path, get_related_term_label
