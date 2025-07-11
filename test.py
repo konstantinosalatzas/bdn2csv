b1c00342d83b63affceecfdc187082fc6f370c9a
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-07-11 13:32:06 +0000

    Add edges to graph

--- test.py
+++ test.py
@@ -427,6 +427,8 @@ class TestUtils(unittest.TestCase):
     def test_check_dag_and_find_cycles(self):
         g = nx.DiGraph()
         g.add_nodes_from(["Warehouse", "Warehouse\\Loading Dock", "Warehouse\\Section"])
+        g.add_edges_from([("Warehouse", "Warehouse\\Loading Dock"), ("Warehouse", "Warehouse\\Section"),
+                          ("Warehouse\\Section", "Warehouse\\Loading Dock"), ("Warehouse\\Loading Dock", "Warehouse\\Section")])
 
 if __name__ == "__main__":
     unittest.main()
