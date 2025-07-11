74764d09004fb57aff7551c0b72c3828af1c8c7f
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-07-11 12:38:38 +0000

    Add type hint

--- bdn2csv/utils.py
+++ bdn2csv/utils.py
@@ -26,6 +26,6 @@ class BDN_dict:
         row = key_to_row.get(key, pd.Series(dtype='float64'))
         return row
 
-def check_dag_and_find_cycles(g):
+def check_dag_and_find_cycles(g: nx.Graph):
     is_dag = nx.is_directed_acyclic_graph(g)
     pass
