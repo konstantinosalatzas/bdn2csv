feac680a1c5a284e3a50b90489916d367a7baf2f
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-07-11 13:14:19 +0000

    Import utility function

--- bdn2csv/__init__.py
+++ bdn2csv/__init__.py
@@ -2,4 +2,4 @@ from bdn2csv.convert import convert
 from bdn2csv.BDN import BDN, add_multiple_value
 from bdn2csv.path2id import path2id, id2name, id2path
 from bdn2csv.viz import visualize, BDNx
-from bdn2csv.utils import BDN_dict, get_parent_path, get_related_term_path, get_related_term_label
+from bdn2csv.utils import BDN_dict, get_parent_path, get_related_term_path, get_related_term_label, check_dag_and_find_cycles
