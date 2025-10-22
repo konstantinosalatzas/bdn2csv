636af6b87c5b1d4fe3926235956c65fadc58ec29
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-10-22 13:49:07 +0000

    Convert in try

--- bdn2csv/batch.py
+++ bdn2csv/batch.py
@@ -7,7 +7,10 @@ def batch(folder_path: str):
     for file in os.scandir(folder_path):
         if file.is_file():
             if file.path.split(".")[1] == "xml":
-                convert(file.path, file.path.replace(".xml", ".csv"))
+                try: # not all XML files are valid to convert
+                    convert(file.path, file.path.replace(".xml", ".csv"))
+                except Exception as error:
+                    print(error)
 
 path = "data"
 batch(path)
