fc45e9817707cd176f7f7a89109ffe8642c423bc
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-10-22 13:37:45 +0000

    Check file extension

--- bdn2csv/batch.py
+++ bdn2csv/batch.py
@@ -4,8 +4,8 @@ import os
 def batch(folder_path: str):
     for file in os.scandir(folder_path):
         if file.is_file():
-            print(file.path)
-            convert(file.path, str(file.path).replace(".xml", ".csv"))
+            if file.path.split(".")[1] == "xml":
+                convert(file.path, str(file.path).replace(".xml", ".csv"))
 
 path = "data"
 batch(path)
