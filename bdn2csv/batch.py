0f520b423d9071d25a2c0891848e02848eadff01
Konstantinos Alatzas <kpalatzas@windowslive.com>
2025-10-24 07:54:34 +0000

    Update batch folder paths

--- bdn2csv/batch.py
+++ bdn2csv/batch.py
@@ -3,14 +3,11 @@
 from bdn2csv.convert import convert
 import os
 
-def batch(in_folder_path: str, out_folder_path: str):
-    for file in os.scandir(in_folder_path):
+def batch(folder_path: str):
+    for file in os.scandir(folder_path):
         if file.is_file():
             if file.path.split(".")[1] == "xml":
                 try: # only BDN export XML files are valid to be converted
                     convert(file.path, file.path.replace(".xml", ".csv"))
-                    print(file.name)
                 except Exception as error:
                     print(error)
-
-batch("data", "data")
